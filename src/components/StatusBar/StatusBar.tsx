cb456e4a2259efb32848fa1161ac65b1a5eb9390
Nick Lemmon <nick.lemmon@gmail.com>
2020-06-11 10:14:06 -0400

    Incorporate gold in to StatusBar

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -13,15 +13,17 @@ const StatusBarWrapper = styled.div`
 
 const StatusBarShape = styled.div`
   position: relative;
+  display: flex;
+  justify-content: flex-start;
   z-index: 2;
-  background-color: ${props => props.theme.colors.offWhite};
+  background-color: ${props => props.theme.colors.white};
   color: ${props => props.theme.colors.darkGray};
   padding: ${props => props.theme.space[3]};
   padding-top: ${props => props.theme.space[2]};
   clip-path: polygon(100% 0, 100% 95%, 99% 100%, 1% 100%, 0 90%, 0 0);
-  border: 1px solid ${props => props.theme.colors.lightGray};
+  border: 1px solid ${props => props.theme.colors.offWhite};
   border-top: 0;
-  box-shadow: inset 0 -5px 0 ${props => props.theme.colors.lightGray};
+  box-shadow: inset 0 -5px 0 ${props => props.theme.colors.offWhite};
 `
 
 const StatusBarShadow = styled.div`
@@ -32,8 +34,9 @@ const StatusBarShadow = styled.div`
   height: 100%;
   width: 100%;
   background-color: ${props => props.theme.colors.white};
-  border-radius: 1rem;
-  filter: drop-shadow(0 10px 20px rgba(0, 0, 0, 0.25));
+  border-radius: 0.5rem;
+  height: 90%;
+  filter: drop-shadow(0 10px 20px rgba(0, 0, 0, 0.2));
 `
 
 export default function StatusBar(props: StatusBarProps) {
