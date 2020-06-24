92955bec300ff6c6a8458b2f15f423435b64e6b3
Nick Lemmon <nick.lemmon@gmail.com>
2020-06-24 18:24:21 -0400

    Fix Banner shadow bug

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -9,10 +9,11 @@ interface BannerProps {
 const BannerWrapper = styled.div`
   z-index: 100;
   width: 100%;
-  box-shadow: 0 0 ${props => props.theme.space[5]} ${props => props.theme.colors.coral};
 `
 
 const BannerShape = styled.div`
+  position: relative;
+  z-index: 2;
   background: ${props =>
     `linear-gradient(0deg, ${props.theme.colors.darkGray}, ${lighten(
       0.125,
@@ -33,6 +34,17 @@ const BannerContent = styled.div`
   font-family: ${props => props.theme.fonts.heading};
 `
 
+const BannerShadow = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  left: 50%;
+  width: 85%;
+  height: 100%;
+  transform: translateX(-50%);
+  box-shadow: 0 0 ${props => props.theme.space[5]} ${props => props.theme.colors.coral};
+`
+
 export default function Banner(props: BannerProps) {
   const { children } = props
 
@@ -41,6 +53,8 @@ export default function Banner(props: BannerProps) {
       <BannerShape>
         <BannerContent>{children}</BannerContent>
       </BannerShape>
+
+      <BannerShadow />
     </BannerWrapper>
   )
 }
