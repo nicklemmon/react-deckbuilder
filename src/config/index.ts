10f529105f32ba4d44db84400ab0baaf65b56a65
Nick Lemmon <nick.lemmon@gmail.com>
2021-02-01 18:45:24 -0500

    Upgrade CRA to version 4.0.1

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ import monsters from './monsters'
 import player from './player'
 import playerPortraits from './playerPortraits'
 
+// eslint-disable-next-line
 export default {
   startingHandCount: 5,
   startingDeck,
