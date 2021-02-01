1d7c355ccfe8f75acfa86b3e8f3438a14171a68d
Nick Lemmon <nick.lemmon@gmail.com>
2021-02-01 09:38:00 -0500

    Adds icon to Select (#40)

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import RoyalDaggerImg from 'src/images/royal-dagger.png'
 import { formFieldStyles } from 'src/styles/helpers'
 import { SelectProps, SelectVariant } from './types'
 
-const SelectElWrapper = styled.div``
+const SelectElWrapper = styled.div`
+  position: relative;
+`
 
-export const SelectEl = styled.select<{ variant: SelectVariant }>`
+const SelectEl = styled.select<{ variant: SelectVariant }>`
   ${formFieldStyles}
+`
+
+const SelectIcon = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: auto;
+  height: 100%;
+  opacity: 0.75;
+  padding: ${props => props.theme.space[2]};
+  pointer-events: none;
+  filter: grayscale(100%);
 `
 
 export default function Select(props: SelectProps) {
@@ -15,6 +30,8 @@ export default function Select(props: SelectProps) {
   return (
     <SelectElWrapper>
       <SelectEl variant={variant} {...rest} />
+
+      <SelectIcon src={RoyalDaggerImg} alt="" role="presentation" />
     </SelectElWrapper>
   )
 }
