a5884f26732abb086b7aa1c4541748edeef82cd9
Nick Lemmon <nick.lemmon@gmail.com>
2021-02-13 16:50:56 -0500

    Add stone druid portrait

--- src/config/playerPortraits/index.ts
+++ src/config/playerPortraits/index.ts
@@ -1,6 +1,7 @@
 import MageImg from 'src/images/hooded-mage.png'
 import KnightImg from 'src/images/knight.png'
 import ArchWizardImg from 'src/images/arch-wizard.png'
+import StoneDruidImg from 'src/images/stone-druid.png'
 
 // eslint-disable-next-line
-export default [MageImg, KnightImg, ArchWizardImg]
+export default [MageImg, KnightImg, ArchWizardImg, StoneDruidImg]
