0ff38c6bad339c6f001e558e4db04d3cd4a80c56
Nick Lemmon <nick.lemmon@gmail.com>
2022-07-30 14:10:58 -0400

    Adds berzerker portrait from DALL-E, removes knight image

--- src/config/playerPortraits/index.ts
+++ src/config/playerPortraits/index.ts
@@ -1,7 +1,7 @@
 import MageImg from 'src/images/hooded-mage.png'
-import KnightImg from 'src/images/knight.png'
+import BerzerkerImg from 'src/images/berzerker.png'
 import ArchWizardImg from 'src/images/arch-wizard.png'
 import StoneDruidImg from 'src/images/stone-druid.png'
 
 // eslint-disable-next-line
-export default [MageImg, KnightImg, ArchWizardImg, StoneDruidImg]
+export default [BerzerkerImg, MageImg, ArchWizardImg, StoneDruidImg]
