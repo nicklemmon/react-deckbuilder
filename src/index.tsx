2b8e88dcd260e5148632103b8bef82f894489354
Nick Lemmon <nick.lemmon@gmail.com>
2023-07-22 10:17:28 -0400

    Adds Vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App'
+
+inject()
 
 ReactDOM.render(<App />, document.getElementById('root'))
