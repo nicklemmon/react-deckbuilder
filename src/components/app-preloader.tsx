974c6d2b35ca77f8f8b78b887eb8f5753610c285
Nick Lemmon <nick.lemmon@gmail.com>
2024-10-09 13:47:59 -0400

    Removes use of styled system

--- src/components/app-preloader.tsx
+++ src/components/app-preloader.tsx
@@ -1,5 +1,3 @@
-import { css } from '../../styled-system/css'
-
 export function AppPreloader() {
-  return <div className={css({ bg: 'red.900' })}>Assets loading</div>
+  return <div>Assets loading</div>
 }
