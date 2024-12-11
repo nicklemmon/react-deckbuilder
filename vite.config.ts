23c49d138dc38a3ac2f215b57bce7d3c3e1d3caf
Nick Lemmon <nick.lemmon@gmail.com>
2024-12-11 18:17:42 -0500

    Trying to use inject plugin but it isn't working

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import injectPreload from 'unplugin-inject-preload/vite'
 import compress from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === 'production' ? compress() : undefined].filter(Boolean),
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.wav'],
+  plugins: [
+    react(),
+    injectPreload({
+      injectTo: 'head',
+      files: [
+        {
+          entryMatch: /.*\.(png|svg)$/,
+          outputMatch: /.*\.(png|svg)$/,
+        },
+      ],
+    }),
+    ...(mode === 'production' ? [compress()] : []),
+  ],
 }))
