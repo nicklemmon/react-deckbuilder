c691f1472f7b8549fdea4c6b2511180e7d013964
Nick Lemmon <nick.lemmon@gmail.com>
2024-12-11 18:23:25 -0500

    Removes inject plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import injectPreload from 'unplugin-inject-preload/vite'
 import compress from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    injectPreload({
-      injectTo: 'head',
-      files: [
-        {
-          entryMatch: /.*\.(png|svg)$/,
-          outputMatch: /.*\.(png|svg)$/,
-        },
-      ],
-    }),
-    ...(mode === 'production' ? [compress()] : []),
-  ],
+  plugins: [react(), ...(mode === 'production' ? [compress()] : [])],
 }))
