04bd7e22a9b2942b221cbc997621324364113911
Nick Lemmon <nick.lemmon@gmail.com>
2025-11-27 19:54:15 -0500

    Adds monkey corn princess and monkey sorcerer witch

---
+++ src/monsters/monkey-sorcerer-witch/config.ts
@@ -0,0 +1,13 @@
+import { defineMonster } from '../../helpers/monsters'
+
+export default defineMonster({
+  name: 'Monkey Sorcerer Witch',
+  level: 7,
+  goldBounty: 5,
+  stats: {
+    maxHealth: 4,
+    health: 9,
+    attack: 12,
+    defense: 1,
+  },
+})
