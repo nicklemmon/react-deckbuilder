ed6de01f774e91810f33c6a750b8614dd6daff40
Nick Lemmon <nick.lemmon@gmail.com>
2026-01-17 09:21:21 -0500

    Character portraits are cached/preloaded instead of imported in the CharacterCreation component

--- src/components/character-creation.tsx
+++ src/components/character-creation.tsx
@@ -1,26 +1,21 @@
 import { type SyntheticEvent } from 'react'
 import { Button } from './button'
 import css from './character-creation.module.css'
-import { resolveModulesWithPaths } from '../helpers/vite'
 import { Stack } from './stack'
 import type { GameMode } from '../types/global'
 import type { CharacterClass } from '../types/character-classes'
-
-const PLAYER_PORTRAIT_MODULES = import.meta.glob('../images/player-portraits/*.(png|webp)', {
-  eager: true,
-})
-
-const PLAYER_PORTRAITS = resolveModulesWithPaths<string>(PLAYER_PORTRAIT_MODULES)
 
 /** UI view for character creation */
 export function CharacterCreation({
   onCreate,
   gameMode,
   characterClasses,
+  playerPortraits,
 }: {
   onCreate: (data: Record<string, FormDataEntryValue>) => void
   gameMode: GameMode
   characterClasses: Array<CharacterClass>
+  playerPortraits: Array<{ path: string; url: string }>
 }) {
   /** Handler for the form submit event */
   const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
@@ -72,23 +67,28 @@ export function CharacterCreation({
           <fieldset className={css['character-portrait-fieldset']}>
             <legend className={css['input-label']}>Character portrait</legend>
 
-            {PLAYER_PORTRAITS.filter((portrait) => {
-              return portrait.path.includes(`.${gameMode}.`)
-            }).map((portrait, index) => {
-              return (
-                <label key={`portrait-ratio-${index}`} className={css['character-portrait-label']}>
-                  <input
-                    name="characterPortrait"
-                    type="radio"
-                    value={portrait.url}
-                    className={css['character-portrait-input']}
-                    defaultChecked={index === 0}
-                  />
+            {playerPortraits
+              .filter((portrait) => {
+                return portrait.path.includes(`.${gameMode}.`)
+              })
+              .map((portrait, index) => {
+                return (
+                  <label
+                    key={`portrait-ratio-${index}`}
+                    className={css['character-portrait-label']}
+                  >
+                    <input
+                      name="characterPortrait"
+                      type="radio"
+                      value={portrait.url}
+                      className={css['character-portrait-input']}
+                      defaultChecked={index === 0}
+                    />
 
-                  <img src={portrait.url} className={css['character-portrait-img']} />
-                </label>
-              )
-            })}
+                    <img src={portrait.url} className={css['character-portrait-img']} />
+                  </label>
+                )
+              })}
           </fieldset>
 
           <Button type="submit">Create character</Button>
