fc89cb6fb3b8ebdf79acdad341925b0922b59225
Nick Lemmon <nick.lemmon@gmail.com>
2026-01-25 08:37:09 -0500

    Integrating music

--- src/components/mode-selection.tsx
+++ src/components/mode-selection.tsx
@@ -1,8 +1,16 @@
+import { useEffect, useRef } from 'react'
+import { Howler } from 'howler'
 import { Button } from './button'
 import { Inline } from './inline'
 import { Panel, PanelBody } from './panel'
 import { Stack } from './stack'
+import hoverMusic from '../sfx/music/music.boogie.wav'
 import styles from './mode-selection.module.css'
+import { getSound } from '../helpers/get-sound'
+
+const boogieMusic = getSound({ src: hoverMusic, volume: 1.0 })
+
+boogieMusic.fade(0, 1, 500)
 
 export function ModeSelection({
   onStandardModeClick,
@@ -21,7 +29,16 @@ export function ModeSelection({
             <Inline>
               <Button onClick={onStandardModeClick}> Standard mode</Button>
 
-              <Button onClick={onRainbowModeClick} className={styles['rainbow-btn']}>
+              <Button
+                onClick={onRainbowModeClick}
+                className={styles['rainbow-btn']}
+                onMouseOver={() => {
+                  boogieMusic.play()
+                }}
+                onMouseOut={() => {
+                  boogieMusic.stop()
+                }}
+              >
                 Rainbow mode
               </Button>
             </Inline>
